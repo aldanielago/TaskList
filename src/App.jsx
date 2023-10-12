cbdfe51d62f6124deefda45de8187b48a8d6303b
aldanielago <aldanielago@gmail.com>
2023-10-11 19:25:39 -0500

    [Functionality] Interaction in TodoItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import "./index.css";
+import { useState } from 'react';
 import { TodoItem } from "./components/TodoItem"
 import { ItemProject } from "./components/ItemProject";
 import { ButtonNewTask } from "./components/buttons/ButtonNewTask";
 import { ButtonViewAllProjects } from "./components/buttons/ButtonViewAllProjects";
 
+const tasks_array = [
+  { text: 'Hacer el ensayo', tasks: 23, completed: true},
+  { text: 'Estudiar biología', tasks: 3, completed: false},
+  { text: 'Codificar la interfaz', tasks: 12, completed: true},
+]
+
 function App() {
+  let [ tasks, setTaks ] = useState(tasks_array);
+  const completedTasks = tasks.filter( task => !!task.completed).length;
+  const totalTasks = tasks.length;
+
   return (
     <section className="pt-4">
       <h1 className="font-Quicksand font-bold text-lg pl-4">Hello user.name</h1>
-      <p className="text-xs pl-4 font-Quicksand text-gray-font">You have work today</p>
+      <p className="text-xs pl-4 font-Quicksand text-gray-font">You have finished {completedTasks} of {totalTasks} taks.</p>
       <h3 className="pt-4 pl-4 font-Quicksand">Today&apos;s taks</h3>
       <div className="pl-4 w-full flex flex-col items-center">
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
+        { tasks.map((task) => (
+          <TodoItem key={task.id} text={task.text} tasks={task.tasks} setTaks={setTaks}/>
+        ))}
         <ButtonNewTask/>
       </div>
       <h3 className="pt-4 pl-4 font-Quicksand">Your projects</h3>
--- src/components/buttons/ButtonViewAllProjects.jsx
+++ src/components/buttons/ButtonViewAllProjects.jsx
@@ -1,7 +1,9 @@
 function ButtonViewAllProjects() {
   return (
     <button
-    className="bg-primary-blue rounded-2xl px-6 py-1 shadow-md text-white my-4 mx-auto hover:bg-blue-hover transition-colors duration-500 ease-in-out">View all projects</button>
+      className="bg-primary-blue rounded-2xl px-6 py-1 shadow-md text-white my-4 mx-auto hover:bg-blue-hover transition-colors duration-500 ease-in-out font-Quicksand">
+      View all projects
+    </button>
   )
 }
 
